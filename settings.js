6492a62e246cbba307d69f07bbc3eb95d1de8201
Rainn108 <ariyowngsff@gmail.com>
2024-06-27 13:53:06 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,25 +3,25 @@
 // Objek untuk menyimpan semua pengaturan profil
 const profileSettings = {
   favicon: "media/profile.jpg", // Path ke favicon
-  title: "Vynaa Valerie | Profile", // Judul halaman
-  metaTitle: "Vynaa || Profile", // Meta title untuk SEO
-  metaDescription: "Profile Website Vynaa Valerie, Creator and Dev", // Deskripsi meta untuk SEO
-  metaKeywords: "Vynaa, Vynaa Valerie, profile Vynaa, siapa Vynaa?", // Keywords meta untuk SEO
+  title: "Ariyo Putra Pratama| Profile", // Judul halaman
+  metaTitle: "Ariyo || Profile", // Meta title untuk SEO
+  metaDescription: "Profile Website Ariyo, Creator and Dev", // Deskripsi meta untuk SEO
+  metaKeywords: "ariyo , aryo wangsaff , siapa aryo?", // Keywords meta untuk SEO
   metaRobots: "index, follow", // Pengaturan robot meta untuk SEO
   imageSrc: "media/profile.jpg", // Path ke gambar profil
-  name: "Vynaa Valerie", // Nama profil
+  name: "Aryo wangsaff", // Nama profil
   occupation: "Creator and Dev", // Pekerjaan atau jabatan
-  location: "Pekanbaru, Riau, Indonesia", // Lokasi
+  location: "Jakarta, Jawa, Indonesia", // Lokasi
   backgroundImage: "media/background.jpg", // Latar belakang halaman
   links: [
-    { title: "GitHub", url: "https://github.com/VynaaValerie", icon: "fa-github" }, // Tautan GitHub
-    { title: "LinkedIn", url: "https://www.linkedin.com/in/vynaaval", icon: "fa-linkedin" }, // Tautan LinkedIn
-    { title: "Twitter", url: "https://twitter.com/vynaaval", icon: "fa-twitter" }, // Tautan Twitter
-    { title: "Instagram", url: "https://www.instagram.com/vynaa_valerie", icon: "fa-instagram" }, // Tautan Instagram
-    { title: "Telegram", url: "https://t.me/VLShop2", icon: "fa-telegram" }, // Tautan Telegram
-    { title: "Email", url: "mailto:vynaavalerie@example.com", icon: "fa-envelope" }, // Tautan Email
-    { title: "WhatsApp", url: "https://wa.me/6282389924037?text=hai", icon: "fa-whatsapp" }, // Tautan WhatsApp
-    { title: "YouTube", url: "https://youtube.com/@VynaaChan", icon: "fa-youtube" } // Tautan YouTube
+    { title: "GitHub", url: "https://github.com/Rainn108", icon: "fa-github" }, // Tautan GitHub
+    { title: "LinkedIn", url: "https://www.linkedin.com/in/Ariyo", icon: "fa-linkedin" }, // Tautan LinkedIn
+    { title: "Twitter", url: "https://twitter.com/Aryowangsaff", icon: "fa-twitter" }, // Tautan Twitter
+    { title: "Instagram", url: "https://www.instagram.com/ariyopratama._?igsh=MWVheWMyeHNoNnBzdA==", icon: "fa-instagram" }, // Tautan Instagram
+    { title: "Telegram", url: "https://t.me/AryoWangsaff", icon: "fa-telegram" }, // Tautan Telegram
+    { title: "Email", url: "mailto: ariyojkt221@gmail.com", icon: "fa-envelope" }, // Tautan Email
+    { title: "WhatsApp", url: "https://wa.me/qr/H3PC7XD6SUNAI1", icon: "fa-whatsapp" }, // Tautan WhatsApp
+    { title: "YouTube", url: "https://youtube.com/@Ariyowangsaff", icon: "fa-youtube" } // Tautan YouTube
   ]
 };
 
